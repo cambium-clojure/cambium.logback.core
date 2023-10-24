50415064fd0d4ceb3ae52a036d35fd453d85e2b6
Michael Gardner <michael@treasuryprime.com>
2023-10-24 13:54:51 -0700

    Add profile for Clojure 1.11

--- project.clj
+++ project.clj
@@ -20,5 +20,6 @@
              :c08 {:dependencies [[org.clojure/clojure  "1.8.0"]]}
              :c09 {:dependencies [[org.clojure/clojure  "1.9.0"]]}
              :c10 {:dependencies [[org.clojure/clojure  "1.10.1"]]}
+             :c11 {:dependencies [[org.clojure/clojure  "1.11.1"]]}
              :dln {:jvm-opts ["-Dclojure.compiler.direct-linking=true"]}}
-  :aliases {"test-all" ["with-profile" "c06,dev:c07,dev:c08,dev:c09,dev:c10,dev" "test"]})
+  :aliases {"test-all" ["with-profile" "c06,dev:c07,dev:c08,dev:c09,dev:c10,dev:c11,dev" "test"]})
